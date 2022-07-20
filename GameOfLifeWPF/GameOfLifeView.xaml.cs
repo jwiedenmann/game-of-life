b6b8732f25f27f61b2b49ae16322c426dfc4c58d
Jonas Wiedenmann <jwiedenmann@exone.de>
2022-07-20 10:05:46 +0200

    added gameoflife view

---
+++ GameOfLifeWPF/GameOfLifeView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GameOfLifeWPF
+{
+    /// <summary>
+    /// Interaction logic for GameOfLifeView.xaml
+    /// </summary>
+    public partial class GameOfLifeView : UserControl
+    {
+        private readonly GameOfLifeViewModel _gameOfLifeViewModel;
+
+        public GameOfLifeView()
+        {
+            InitializeComponent();
+
+            _gameOfLifeViewModel = new();
+            DataContext = _gameOfLifeViewModel;
+
+        }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CompositionTarget.Rendering += CompositionTarget_Rendering; ;
+        }
+
+        private void CompositionTarget_Rendering(object? sender, System.EventArgs e)
+        {
+            gameImage.Source = _gameOfLifeViewModel.RenderImage();
+        }
+    }
+}
