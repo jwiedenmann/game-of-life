08139e283f7aaf4c51c78d6ece1dd0644884acc0
Jonas Wiedenmann <jwiedenmann@exone.de>
2022-07-20 10:05:20 +0200

    removed code from mainwindow

--- GameOfLifeWPF/MainWindow.xaml.cs
+++ GameOfLifeWPF/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GameOfLifeWPF
 {
@@ -24,33 +10,6 @@ namespace GameOfLifeWPF
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        public GameDrawer GameDrawer { get; set; }
-        public GameController GameController { get; set; }
-
-        private void Render(object? sender, EventArgs e)
-        {
-            gameImage.Source = GameDrawer?.Draw(GameController.Field);
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            GameDrawer = new GameDrawer((int)borderCanvas.ActualWidth, (int)borderCanvas.ActualHeight);
-            GameController = new GameController(GameDrawer.HorizontalCells, GameDrawer.VerticalCells);
-
-            CompositionTarget.Rendering += Render;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            GameController.RandomiseField();
-            GameController.Start();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            GameController.Stop();
         }
     }
 }
